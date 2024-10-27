5c0f0c2f22729dab8183c1180361bd0c1fe715e1
bai_777 <bai_777@mail.ru>
2024-10-27 19:26:24 +0300

    Добавил текстовую метку

--- test.py
+++ test.py
@@ -2,5 +2,11 @@ from tkinter import *
 
 window = Tk()
 window.title('Window input text')
+window.geometry('400x400')
+
+labelWindow = Label(text='Window input')
+labelWindow.pack()
+
+
 window.mainloop()
 
